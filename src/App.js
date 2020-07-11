ca86b8d82cc86a32567efa0e18a18d2262c20d06
Leonardo Reina <leonardo@formahomolog.com.br>
2020-07-11 13:13:21 -0300

    Starting challenge adding some imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import "./styles.css";
 
 function App() {
+
+
   async function handleAddRepository() {
     // TODO
+    axios.get('/repositories')
   }
 
   async function handleRemoveRepository(id) {
